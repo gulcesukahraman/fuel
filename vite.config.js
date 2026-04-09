25955371f94e9296cd8d845b58c57e87937b6a33
gulcesukahraman <gulcesukahraman@hotmail.com>
2026-04-09 18:45:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // GitHub Pages'teki alt klasör adın (depo adın) buraya gelmeli
-  base: '/fuel/', 
-  
-  build: {
-    // Çıktı klasörü
-    outDir: 'dist',
-  },
-  server: {
-    // Yerel çalışırken kolaylık sağlar
-    open: true,
-  }
+  base: '/fuel/',
 })
